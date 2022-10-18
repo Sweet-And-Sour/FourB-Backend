eadff552fac144b95863af38489304879317cb1d
Cardroid <number1dh@gmail.com>
2022-10-18 10:44:31 +0900

    Feat: API 문서 자동생성 기능 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('The FourB backend')
+    .setDescription('FourB API 설명 테스트')
+    .setVersion('0.1')
+    .build();
+  app.setGlobalPrefix('api');
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('dev', app, document);
+
   await app.listen(3000);
 }
 bootstrap();

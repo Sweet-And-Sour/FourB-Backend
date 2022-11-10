1501b536d0e302b9932f616da302580b5d252dd8
YeJun, Jung <yejun614@naver.com>
2022-11-10 15:14:34 +0900

    Feat: 유저 정보 수정을 위한 update 기능 추가

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -1,21 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ConnectionService } from 'src/services/connection/connection.service';
 import { UserData } from 'src/interfaces/user-data.interface';
 
 @Injectable()
 export class UserService {
+  private readonly logger = new Logger(UserService.name);
   constructor(private connectionService: ConnectionService) {}
 
+  async userExist(username: string): Promise<boolean> {
+    try {
+      const [rows] = await this.connectionService.pool.execute(
+        'SELECT username FROM Users WHERE username=?',
+        [username],
+      );
+
+      return (rows as any).length === 1;
+    } catch (e) {
+      this.logger.error(e);
+    }
+
+    return false;
+  }
+
   async create(data: UserData) {
-    this.connectionService.pool.getConnection((error, connection) => {
+    const query = `INSERT INTO Users (username, password, email) VALUES ("${data.username}", "${data.password}", "${data.email}")`;
+
+    this.connectionService.pool.query(query, (error) => {
       if (error) throw error;
-
-      const query = `INSERT INTO Users (username, password, email) VALUES ("${data.username}", "${data.password}", "${data.email}")`;
-      connection.query(query, (error, results, fields) => {
-        if (error) throw error;
-
-        connection.release();
-      });
     });
+  }
+
+  async update(data: UserData) {
+    if (!(await this.userExist(data.username))) {
+      this.logger.warn('UserService.update: the username is not exists');
+      return false;
+    }
+
+    try {
+      this.connectionService.pool.execute(
+        'UPDATE Users SET email=?, password=? WHERE username=?',
+        [data.email, data.password, data.username],
+      );
+    } catch (e) {
+      return false;
+    }
+
+    return true;
   }
 }

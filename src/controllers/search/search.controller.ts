56c8da3eea39ee3d11cae9dc4a61aed6b0b2a3c9
Cardroid <number1dh@gmail.com>
2022-12-11 07:07:59 +0900

    Feat: 사용자 검색 기능 추가

--- src/controllers/search/search.controller.ts
+++ src/controllers/search/search.controller.ts
@@ -1,22 +1,52 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { SearchService } from '../../services/search/search.service';
 
 @ApiTags('Search')
 @Controller('search')
 export class SearchController {
+  constructor(private searchService: SearchService) {}
+
+  @ApiOperation({ summary: '유저 검색' })
+  @ApiQuery({
+    name: 'username',
+    required: false,
+    type: String,
+  })
+  @ApiQuery({
+    name: 'page',
+    required: false,
+    example: 1,
+    type: Number,
+  })
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+    example: 20,
+    type: Number,
+  })
   @Get('/users')
-  searchUser(): string {
-    return '유저 찾기';
+  async searchUsers(
+    @Query('username') username: undefined | string = undefined,
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 20,
+  ) {
+    const results = await this.searchService.searchUsers(
+      (username = username),
+      (page = page),
+      (limit = limit),
+    );
+
+    return {
+      message: '유저 검색',
+      success: results !== undefined,
+      results: results,
+    };
   }
 
   @Get('/teams')
   searchTeam(): string {
     return '팀 찾기';
-  }
-
-  @Get('/files')
-  searchFile(): string {
-    return '파일 찾기';
   }
 
   @Get('/contents')
